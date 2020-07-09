65a3102891499846bf98d553fdaacdba3516becd
juliana <choisjuliana@gmail.com>
2020-07-08 23:24:01 -0500

    Index code with pug format added

---
+++ public/javascripts/map.js
@@ -0,0 +1,10 @@
+var mymap = L.map('main_map').setView([10.988176, -74.788774], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(mymap);
