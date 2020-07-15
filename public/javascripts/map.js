d335d2ced1fba469fb7e1acd24f50808a89f5d31
juliana <choisjuliana@gmail.com>
2020-07-15 12:58:38 -0500

    map fixed

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,10 +1,8 @@
-var mymap = L.map('main_map').setView([10.988176, -74.788774], 13);
+var map = L.map('main_map', {
+    center: [51.505, -0.09],
+    zoom: 13
+});
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+}).addTo(map)
